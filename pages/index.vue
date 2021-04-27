f9b84214bf73ea6d535582c768ea4e87c0159fc2
Alexandre LAM <alexandrelam@outlook.com>
2021-04-27 17:54:36 +0200

    can now restart and know what player is palying

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
+    <h1>C'est à {{ currentPlayer }} de jouer !</h1>
     <Board />
+    <button @click="restart">Restart</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Board from "~/components/Board.vue";
+import { mapMutations } from "vuex";
 
 export default Vue.extend({
   components: { Board },
+  computed: {
+    currentPlayer() {
+      return this.$store.state.player;
+    },
+  },
+  methods: {
+    ...mapMutations(["restart"]),
+  },
 });
 </script>
--- store/index.js
+++ store/index.js
@@ -15,7 +15,7 @@ export const mutations = {
     let tmpColumn = state.board[column];
     if (tmpColumn[row] == " ") {
       // if you can play then play the move
-      tmpColumn[row] = state.player;
+      Vue.set(tmpColumn, row, state.player);
       Vue.set(state.board, column, tmpColumn);
 
       // then toggle the player
@@ -25,5 +25,13 @@ export const mutations = {
         state.player = "X";
       }
     }
+  },
+  restart(state) {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        Vue.set(state.board[i], j, " ");
+      }
+    }
+    state.player = "X";
   },
 };
